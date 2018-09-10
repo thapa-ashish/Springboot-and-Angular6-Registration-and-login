5660fb69b4025a93bc11f39de75332196730c64c
wizard0046 <ashishthapa000@gmail.com>
2018-09-10 12:32:27 -0400

    server files added

---
+++ springboot-server/src/main/java/com/springang/demo/springangular/SpringangularApplication.java
@@ -0,0 +1,12 @@
+package com.springang.demo.springangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringangularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringangularApplication.class, args);
+	}
+}
---
+++ springboot-server/src/test/java/com/springang/demo/springangular/SpringangularApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springang.demo.springangular;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringangularApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
